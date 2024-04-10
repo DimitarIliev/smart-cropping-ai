67c00342e80355677f3e5bcd43b50dc45719ead4
Dimitar Iliev <diki_tazo@hotmail.com>
2024-04-10 23:45:26 +0200

    Add project files.

---
+++ SmartCropping/Program.cs
@@ -0,0 +1,19 @@
+﻿// Add your Computer Vision key and endpoint
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+var key = "<your-azure-ai-vision-key>";
+var endpoint = "<your-azure-ai-vision-endpoint>";
+
+var imageUrl = "<your-image-url>";
+
+// Create a client
+var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+
+var response = await client.GenerateThumbnailAsync(200, 200, imageUrl, smartCropping: false);
+
+// Save thumbnail image
+string thumbnailFileName = "thumbnail.png";
+using (Stream thumbnailFile = File.Create(thumbnailFileName))
+{
+    response.CopyTo(thumbnailFile);
+}
